d5fbf47d17307b289a721c9d37209f81853dcffd
Austin LaShure <austinglashure@gmail.com>
2021-06-24 22:51:50 -0400

    built while loop so it can take different input without resetting the program

--- port.c
+++ port.c
@@ -5,5 +5,21 @@
 #define VAPOR_MOLAR_MASS = 0.018016 // kg/mol
 
 int main(){
+    int running = 1;
+
+    while (running > 0){
+        // put statements and queries for user here
+        puts("Do you want to reset the loop?\ny/n");
+        char reset[1];
+        gets(reset);
+        if (*reset > 120){// can use ascii values to compart chars
+            running = 1;
+        }
+        else {
+            running = 0;
+        }
+    }
+    puts("I'm out");
+
     return 0;
 }
