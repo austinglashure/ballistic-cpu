402491caf2bac63fa78bd5eeee63a7dd520123aa
Austin LaShure <austinglashure@gmail.com>
2021-06-25 17:14:29 -0400

    created conversion functions for celcius and Kelvin;

--- port.c
+++ port.c
@@ -4,15 +4,25 @@
 #define AIR_MOLAR_MASS = 0.0289654 // kg/mol
 #define VAPOR_MOLAR_MASS = 0.018016 // kg/mol
 
+float toCelcius(float fahr){
+    return (fahr - 32) / 1.8;
+}
+
+float toKelvin(float celc){
+    return celc + 273.15;
+}
+
 int main(){
     int running = 1;
     int reset = 1;
 
     while (running > 0){
-        int fahrenheit;
+        float fahrenheit, celcius, kelvin;
         printf("What's the temperature in Farenheit?\n");
-        scanf("%d", &fahrenheit);
-        printf("The temperature is %d degrees\n", fahrenheit);
+        scanf("%f", &fahrenheit);
+        // printf("The temperature is %f degrees\n", fahrenheit);
+        celcius = toCelcius(fahrenheit);
+        kelvin = toKelvin(celcius);
         // put statements and queries for user here
         printf("Do you want to reset the loop? 1/0\n");
         scanf("%d", &reset);
