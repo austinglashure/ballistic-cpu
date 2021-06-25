9559394081a3548aab646de08a3aa136d9f4e6e6
Austin LaShure <austinglashure@gmail.com>
2021-06-24 22:36:11 -0400

    defined immutable constants for calculations molar mass of air molar mass of water vapor universal gas constant

--- port.c
+++ port.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+#define UNIVERSAL_GAS_CONSTANT = 8.312 // J/(K*mol)
+#define AIR_MOLAR_MASS = 0.0289654 // kg/mol
+#define VAPOR_MOLAR_MASS = 0.018016 // kg/mol
+
 int main(){
     return 0;
 }
