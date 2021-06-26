0ef3cd8eb2ba3c39ba928a3b506f22358ff81da3
Austin LaShure <austinglashure@gmail.com>
2021-06-26 00:53:57 -0400

    added saturation vapor pressure variable and function

--- port.c
+++ port.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <math.h>
 
 #define UNIVERSAL_GAS_CONSTANT = 8.312 // J/(K*mol)
 #define AIR_MOLAR_MASS = 0.0289654 // kg/mol
@@ -20,21 +21,36 @@ float toHectoPascal(float pasc){
     return pasc * 0.01;
 }
 
+float getSaturationVaporPressure(float celc){
+    float exponent = (7.5 * celc) / (celc + 237.3);
+    return  6.1078 * 100 * pow(10, exponent);
+}
+
 int main(){
     int running = 1;
     int reset = 1;
 
     while (running > 0){
+        // temperature variables
         float fahrenheit, celcius, kelvin;
+        // pressure variables
         float inHg, pascals, hectopascals;
+        // get temperature input
         printf("What's the temperature in Farenheit?\n");
         scanf("%f", &fahrenheit);
+        // do temperature conversions
         celcius = toCelcius(fahrenheit);
         kelvin = toKelvin(celcius);
+        // get pressure input
         printf("What is the pressure in inHg?\n");
         scanf("%f", &inHg);
+        // do pressure conversions
         pascals = toPascal(inHg);
         hectopascals = toHectoPascal(pascals);
+        // air density calculations
+        float saturation_vapor_pressure;
+        saturation_vapor_pressure = getSaturationVaporPressure(celcius);
+
 
         // put statements and queries for user here
         printf("Do you want to reset the loop? 1/0\n");
