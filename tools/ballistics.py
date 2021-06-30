7f5d9dd4d510e0b83baa599aa3bb382e57fa65ce
Austin LaShure <austinglashure@gmail.com>
2021-06-30 16:13:35 -0400

    put the individual classes in their own folder

---
+++ tools/ballistics.py
@@ -0,0 +1,38 @@
+TIME_IMPULSE = 0.001
+
+
+class Flight():
+    def __init__(self, meters_secs, drag_coefficient, range, bullet_mass):
+        self.vel0 = meters_secs
+        self.drag_coef = drag_coefficient
+        self.range = range
+        self.mass = bullet_mass
+
+    def calculateInstantaneousForce(self):
+        self.instant_drag_force = self.drag_coef * self.vel0 * self.vel0  # pow(self.vel0, 2)
+
+    def calculateInstantDeceleration(self):
+        self.calculateInstantaneousForce()
+        self.instant_decel = -1 * (self.instant_drag_force / self.mass)
+    
+    def calculateVelocityFinal(self):
+        self.calculateInstantDeceleration()
+        self.vel_final = self.vel0 + (self.instant_decel * TIME_IMPULSE)
+    
+    def calculateDisplacement(self):
+        self.calculateVelocityFinal()
+        avg = (self.vel0 + self.vel_final) / 2.0
+        self.displacement = avg * TIME_IMPULSE
+    
+    def calculateFlightTime(self):
+        foo = 0.0
+        self.flight_time = 0.0
+        while foo < self.range:
+            self.calculateDisplacement()
+            foo += self.displacement
+            self.flight_time += TIME_IMPULSE
+            self.vel0 = self.vel_final
+            
+    def calculateBulletDrop(self):
+        self.calculateFlightTime()
+        self.bullet_drop = -4.9 * pow(self.flight_time, 2)
---
+++ tools/platform.py
@@ -0,0 +1,23 @@
+import math
+
+class Platform():
+    def __init__(self, bc, cal_inches, muzzle_vel_ft, grains):
+        self.bc = bc
+        self.cal_inches = cal_inches
+        self.muzzle_vel_ft = muzzle_vel_ft
+        self.grains = grains
+        # SI unit mass conversion
+        self.mass = self.grains / 15432
+        # SI unit velocity conversion
+        self.muzzle_vel = muzzle_vel_ft / 3.2808398950131
+        # SI unit caliber conversion
+        self.caliber = self.cal_inches * 0.0254
+
+    def calculateCrossSectionalArea(self):
+        radius = self.caliber / 2
+        self.cross_sectional_area = math.pi * pow(radius, 2)
+    
+    def calculateDragCoefficient(self, air_density):
+        self.calculateCrossSectionalArea()
+        self.drag_coef = (0.5 * air_density * self.bc *
+                          self.cross_sectional_area)
