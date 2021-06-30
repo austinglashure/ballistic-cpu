3715d7c97fe78f05fcd709a53fa13cce60a1faf2
Austin LaShure <austinglashure@gmail.com>
2021-06-30 16:15:31 -0400

    changed import to new class file location

--- range.py
+++ range.py
@@ -1,6 +1,6 @@
-import weather as w
-import platform as p
-import ballistics as b
+from tools import weather as w
+from tools import platform as p
+from tools import ballistics as b
 
 fahr, inHg, hum = 85, 30, 100
 conditions = w.Weather(fahr, inHg, hum)
@@ -8,10 +8,8 @@ conditions = w.Weather(fahr, inHg, hum)
 bc, cal, muzz_vel, grains = 0.45, 0.308, 2821.5, 165
 mine = p.Platform(bc, cal, muzz_vel, grains)
 mine.calculateDragCoefficient(conditions.air_density)
-print(mine.drag_coef)
+
 
 shot = b.Flight(mine.muzzle_vel, mine.drag_coef, 500, mine.mass)
 shot.calculateBulletDrop()
-print(shot.bullet_drop)  # -3.05809
-print(shot.flight_time)  # 0.79
-
+print(shot.bullet_drop)
