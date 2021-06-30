3abc8b9a87e483427f4e991b705d9043db33a8c9
Austin LaShure <austinglashure@gmail.com>
2021-06-30 01:26:02 -0400

    all classes import into the main file fine

--- range.py
+++ range.py
@@ -1,7 +1,14 @@
 import weather as w
+import platform as p
+import ballistics as b
 
 fahr, inHg, hum = 85, 30, 100
-
 conditions = w.Weather(fahr, inHg, hum)
+# .308 weapon profile
+bc, cal, muzz_vel, grains = 0.45, 0.308, 2821.5, 165
+mine = p.Platform(bc, cal, muzz_vel, grains)
+mine.calculateDragCoefficient(conditions.air_density)
 
-
+shot = b.Flight(mine.muzzle_vel, mine.drag_coef, 500, mine.mass)
+shot.calculateBulletDrop()
+print(shot.bullet_drop)
