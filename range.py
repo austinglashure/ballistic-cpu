d5ba736398675945c375eb5a713e7584f504edc5
Austin LaShure <austinglashure@gmail.com>
2021-06-30 14:00:11 -0400

    the ballistics package works great

--- range.py
+++ range.py
@@ -8,7 +8,10 @@ conditions = w.Weather(fahr, inHg, hum)
 bc, cal, muzz_vel, grains = 0.45, 0.308, 2821.5, 165
 mine = p.Platform(bc, cal, muzz_vel, grains)
 mine.calculateDragCoefficient(conditions.air_density)
+print(mine.drag_coef)
 
 shot = b.Flight(mine.muzzle_vel, mine.drag_coef, 500, mine.mass)
 shot.calculateBulletDrop()
-print(shot.bullet_drop)
+print(shot.bullet_drop)  # -3.05809
+print(shot.flight_time)  # 0.79
+
