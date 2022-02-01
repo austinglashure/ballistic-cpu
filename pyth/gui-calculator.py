c29e7f0b465e6ee297fd5707df4eed1f8a96aa4d
Austin LaShure <austinglashure@gmail.com>
2022-02-01 00:24:09 -0500

    refactoring code

--- pyth/gui-calculator.py
+++ pyth/gui-calculator.py
@@ -1,15 +1,23 @@
 import balls # imports everything in that folder
 import tkinter as tk
 
+# triggered when calculate button is pressed
 def on_calculate_press():
     print("You pressed the \"Calculate\" button")
 
 window = tk.Tk()
+# style the window
 window.title("Ballistic CPU")
-title_label = tk.Label(window, text="Go Ballistic!")
-title_label.pack()
 window.geometry("500x700")  # widthxheight sets window size
-calculate_button = tk.Button(window, text="Calculate", bg="red", fg="white", width=50, height=2, command=on_calculate_press)
+# calculate button creation
+calculate_button = tk.Button(window,
+                             text="Calculate",
+                             bg="red",
+                             fg="white",
+                             width=50,
+                             height=2,
+                             command=on_calculate_press)
 calculate_button.pack()
+
 window.mainloop()
     
