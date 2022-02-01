c4bac67b4480712cb4b1878c7b72a43db130f6bf
Austin LaShure <austinglashure@gmail.com>
2022-02-01 00:05:32 -0500

    changed the size of the window, and added a dynamic button to prompt what will eventually be calculations leading to bullet drop

--- pyth/gui-calculator.py
+++ pyth/gui-calculator.py
@@ -1,8 +1,15 @@
 import balls # imports everything in that folder
 import tkinter as tk
 
+def on_calculate_press():
+    print("You pressed the \"Calculate\" button")
+
 window = tk.Tk()
 window.title("Ballistic CPU")
-tit_lable = tk.Label(window, text="Go Ballistic!").pack()
+title_label = tk.Label(window, text="Go Ballistic!")
+title_label.pack()
+window.geometry("500x700")  # widthxheight sets window size
+calculate_button = tk.Button(window, text="Calculate", bg="red", fg="white", width=50, height=2, command=on_calculate_press)
+calculate_button.pack()
 window.mainloop()
     
